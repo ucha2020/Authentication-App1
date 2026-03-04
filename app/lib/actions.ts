3f8ba3ba844c5c8bf48b578579d83a2f9b569e78
ucha <uchaokwudili@gmaul.com>
2026-03-05 00:00:45 +0100

    Edited parameter type on creatUser function

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -2,6 +2,7 @@
 
 import { revalidatePath } from "next/cache";
 import { redirect } from "next/navigation";
+import { Prisma } from "@/generated/prisma/client";
 import { signIn } from "@/lib/auth";
 import { AuthError } from "next-auth";
 import { z } from "zod";
@@ -9,12 +10,11 @@ import { credentialsSchema } from "@/lib/zod";
 import type { CreateUserState } from "@/app/typeShapes";
 import { createNewUser } from "@/lib/queryDB";
 import bcrypt from "bcryptjs";
-import { promises } from "dns";
 
 export async function createUser(
-  prevState: string,
+  prevState: string | undefined,
   formData: FormData,
-): Promise<CreateUserState | string | void> {
+) {
   const scannedCredentials = credentialsSchema.safeParse({
     email: formData.get("email"),
     password: formData.get("password"),
@@ -24,17 +24,20 @@ export async function createUser(
   const error = scannedCredentials.error;
 
   if (!success) {
-    return {
-      errors: `${error}`,
-    };
+    return "Something went wrong.";
   }
 
-  const hashedPassword = (await data) && bcrypt.hash(data.password, 10);
+  //const hashedPassword = (await data) && bcrypt.hash(data.password, 10);
   let newUser;
   try {
-    newUser = await createNewUser(data);
-  } catch (error: any) {
-    if (error.code === "P2002") {
+    if (data) {
+      newUser = await createNewUser(data);
+    }
+  } catch (error: unknown) {
+    if (
+      error instanceof Prisma.PrismaClientKnownRequestError &&
+      error.code === "P2002"
+    ) {
       return "Email already registered";
     }
     return "Something went wrong.";
@@ -42,8 +45,8 @@ export async function createUser(
 
   try {
     await signIn("credentials", {
-      email: newUser.email,
-      password: newUser.password,
+      email: newUser?.email,
+      password: newUser?.password,
       redirectTo: "/dashboard",
     });
   } catch (error) {
@@ -77,7 +80,10 @@ export async function authenticateUserWithCredencials(
   }
 }
 
-export const authenticateUserWithGitHub = async () => {
+export const authenticateUserWithGitHub = async (
+  prevState: string | undefined,
+  formData: FormData,
+) => {
   try {
     await signIn("github", { redirectTo: "/dashboard" });
   } catch (error) {
@@ -85,6 +91,7 @@ export const authenticateUserWithGitHub = async () => {
     // not existing, or the user not having the correct role.
     // In some cases, you may want to redirect to a custom error
     if (error instanceof AuthError) {
+      return "Something went wrong.";
       //return redirect(`${SIGNIN_ERROR_URL}?error=${error.type}`);
     }
 
@@ -95,108 +102,3 @@ export const authenticateUserWithGitHub = async () => {
     throw error;
   }
 };
-/*const FormSchema = z.object({
-  id: z.string(),
-  customerId: z.string({
-    invalid_type_error: 'Please select a customer.',
-  }),
-  amount: z.coerce
-    .number()
-    .gt(0, { message: 'Please enter an amount greater than $0.' }),
-  status: z.enum(['pending', 'paid'], {
-    invalid_type_error: 'Please select an invoice status.',
-  }),
-  date: z.string(),
-});*
-
-const CreateInvoice = FormSchema.omit({ id: true, date: true });
-const UpdateInvoice = FormSchema.omit({ date: true, id: true });
-
-export type State = {
-  errors?: {
-    customerId?: string[];
-    amount?: string[];
-    status?: string[];
-  };
-  message?: string | null;
-};
-
-export async function createInvoice(prevState: State, formData: FormData) {
-  // Validate form fields using Zod
-  const validatedFields = CreateInvoice.safeParse({
-    customerId: formData.get('customerId'),
-    amount: formData.get('amount'),
-    status: formData.get('status'),
-  });
-
-  // If form validation fails, return errors early. Otherwise, continue.
-  if (!validatedFields.success) {
-    return {
-      errors: validatedFields.error.flatten().fieldErrors,
-      message: 'Missing Fields. Failed to Create Invoice.',
-    };
-  }
-
-  // Prepare data for insertion into the database
-  const { customerId, amount, status } = validatedFields.data;
-  const amountInCents = amount * 100;
-  const date = new Date().toISOString().split('T')[0];
-
-  // Insert data into the database
-  try {
-    await sql`
-      INSERT INTO invoices (customer_id, amount, status, date)
-      VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
-    `;
-  } catch (error) {
-    // If a database error occurs, return a more specific error.
-    return {
-      message: 'Database Error: Failed to Create Invoice.',
-    };
-  }
-
-  // Revalidate the cache for the invoices page and redirect the user.
-  revalidatePath('/dashboard/invoices');
-  redirect('/dashboard/invoices');
-}
-
-export async function updateInvoice(
-  id: string,
-  prevState: State,
-  formData: FormData,
-) {
-  const validatedFields = UpdateInvoice.safeParse({
-    customerId: formData.get('customerId'),
-    amount: formData.get('amount'),
-    status: formData.get('status'),
-  });
-
-  if (!validatedFields.success) {
-    return {
-      errors: validatedFields.error.flatten().fieldErrors,
-      message: 'Missing Fields. Failed to Update Invoice.',
-    };
-  }
-
-  const { customerId, amount, status } = validatedFields.data;
-  const amountInCents = amount * 100;
-
-  try {
-    await sql`
-      UPDATE invoices
-      SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
-      WHERE id = ${id}
-    `;
-  } catch (error) {
-    return { message: 'Database Error: Failed to Update Invoice.' };
-  }
-
-  revalidatePath('/dashboard/invoices');
-  redirect('/dashboard/invoices');
-}
-
-export async function deleteInvoice(id: string) {
-  await sql`DELETE FROM invoices WHERE id = ${id}`;
-  revalidatePath('/dashboard/invoices');
-}
-*/
